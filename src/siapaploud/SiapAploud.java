711e1e6ef9ec63bd12ecfc159151da48eab1e03e
yoga1233 <yoga.cncy11@gmail.com>
2017-05-19 00:53:34 +0700

    test

--- src/siapaploud/SiapAploud.java
+++ src/siapaploud/SiapAploud.java
@@ -15,7 +15,7 @@ public class SiapAploud {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("hello");
+        System.out.println("tst package");
     }
     
 }
