c0eeb285aa422d7ac21be7f41af9bd7d3bd29d06
user all <user all@userall-PC>
2017-05-19 12:58:24 +0700

    DataAwal

---
+++ src/siapaploud/Data.java
@@ -0,0 +1,99 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package siapaploud;
+
+/**
+ *
+ * @author user all
+ */
+public class Data {
+    private String NIK;
+    private String nama;
+    private String TTL;
+    private String Jkl;
+    private String alamat;
+    private String rt;
+    private String desa;
+    private String kecamatan;
+    private String agama;
+    private String status;
+    private String pekerjaan;
+    private String kewarganegaraan;
+    
+    public String getNIK(){
+        return NIK;
+    }
+    public void setNIK(String NIK){
+        this.NIK = NIK;
+    }
+    public String getnama(){
+        return nama;
+    }
+    public void setnama(String nama){
+        this.nama = nama;
+    }
+    public String getTTL(){
+        return TTL;
+    }
+    public void setTTL(String TTL){
+        this.TTL = TTL;
+    }
+    public String getJkl(){
+        return Jkl;
+    }
+    public void setJkl(String Jkl){
+        this.Jkl = Jkl;
+    }
+    public String getalamat(){
+        return alamat;
+    }
+    public void setalamat(String alamat){
+        this.alamat = alamat;
+    }
+    public String getrt(){
+        return rt;
+    }
+    public void setrt(String rt){
+        this.rt = rt;
+    }
+    public String getdesa(){
+        return desa;
+    }
+    public void setdesa(String desa){
+        this.desa = desa;
+    }
+    public String getkecamatan(){
+        return kecamatan;
+    }
+    public void setkecamatan(String kecamatan){
+        this.kecamatan = kecamatan;
+    }
+    public String getagama(){
+        return agama;
+    }
+    public void setagama(String agama){
+        this.agama = agama;
+    }
+    public String getstatus(){
+        return status;
+    }
+    public void getstatus(String status){
+        this.status = status;
+    }
+    public String getpekerjaan(){
+        return pekerjaan;
+    }
+    public void setpekerjaan(String pekerjaan){
+        this.pekerjaan = pekerjaan;
+    }
+    public String getkewarganegaraan(){
+        return kewarganegaraan;
+    }
+    public void setkewarganegaraan(String kewarganegaraan){
+        this.kewarganegaraan = kewarganegaraan;
+    }
+    
+    }
